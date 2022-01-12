bcecc4ced7e7982428ddd7a03372d33b69a349f0
fascinate98 <fascinate982@naver.com>
2022-01-12 19:36:19 +0900

    prob5

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,17 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+
+		for(int i = 1; i <= 99; i++) {
+			if(i % 3 == 0 ) {
+				System.out.print(i + "짝");
+				if((i / 10) % 3 == 0 && i > 10) {
+					if(i % 10 != 0)
+						System.out.print("짝");
+				}
+				System.out.println("\n");
+			}
+		}
+		
 	}
 }
