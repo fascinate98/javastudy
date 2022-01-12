205b42e140596540f02ae5cd29a3c9e263fa0233
fascinate98 <fascinate982@naver.com>
2022-01-12 19:19:20 +0900

    prob1

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -7,6 +7,17 @@ public class Prob1 {
 		Scanner scanner = new Scanner( System.in );
 		
 		/* 코드 작성 */
+		int i;
+		System.out.println("수를 입력하세요 :");
+		i = scanner.nextInt();
+		
+		if(i % 3 == 0) {
+			System.out.println("3의 배수입니다");
+		}else {
+			System.out.println("3의 배수가 아닙니다");
+		}
+		
+		
 		
 		scanner.close();
 	}
