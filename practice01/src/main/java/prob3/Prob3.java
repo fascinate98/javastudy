7697c7ca0d09ac21c5ff1388b980ad7ab2329e0b
fascinate98 <fascinate982@naver.com>
2022-01-12 19:26:44 +0900

    prob3

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -6,9 +6,28 @@ public class Prob3 {
 	
 	public static void main(String[] args) {
 
+		System.out.print("숫자를 입력하세요:");
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		int a = scanner.nextInt();
+		int result = 0;
+		
+		if(a % 2 == 0) {
+			for(int i = 0; i <= a; i ++) {
+				if(i % 2 == 0) {
+					result += i;
+				}
+			}
+		
+		}else {
+			for(int i = 0; i <= a; i ++) {
+				if(i % 2 == 1) {
+					result += i;
+				}
+			}
+		}
+		System.out.println("결과 값 : " + result);
 		
 		scanner.close();
 	}
