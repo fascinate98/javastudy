43b4a03e781fa2489664458261388b1ef782e19f
fascinate98 <fascinate982@naver.com>
2022-01-12 19:28:53 +0900

    prob4

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -9,7 +9,12 @@ public class Prob4 {
 
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
-
+		String rs = "";
+		
+		for(int i = 0; i < text.length(); i++) {
+			rs += text.charAt(i);
+			System.out.println(rs);
+		}
 		
 		scanner.close();
 	}
