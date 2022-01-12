2787fcb97af692146cf8033e5fe7613c57a49c0d
fascinate98 <fascinate982@naver.com>
2022-01-12 21:22:40 +0900

    practice04 prob05

--- practice04/src/main/java/prob04/Depart.java
+++ practice04/src/main/java/prob04/Depart.java
@@ -1,11 +1,16 @@
 package prob04;
 
 public class Depart extends Employee{
-	private String dep;
+	private String department;
 	
 	Depart(String name, int salary, String dep) {
 		super.setName(name); 
 		super.setSalary(salary);
-		this.dep = dep;
+		this.department = dep;
+	}
+	
+	public void getInformation() {
+		System.out.printf("이름: %s   연봉: %d   부서:%s\n", super.getName(), super.getSalary(), department);
+
 	}
 }
