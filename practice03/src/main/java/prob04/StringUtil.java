2d8df8955b4a0790006156776964ff3c1391b1ec
fascinate98 <fascinate982@naver.com>
2022-01-12 20:47:42 +0900

    practice04 prob04

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,14 @@
 package prob04;
 
 public class StringUtil {
+	
+	public static String concatenate(String[] str) {
+		String rs = "";
+		
+		for(int i = 0; i < str.length; i++) {
+			 rs += str[i];
+		}
+		
+		return rs;
+	}
 }
