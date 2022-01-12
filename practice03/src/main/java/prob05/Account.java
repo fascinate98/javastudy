ffe64e8395c5cf6f0a42566b764fa59bd839328c
fascinate98 <fascinate982@naver.com>
2022-01-12 20:52:57 +0900

    practice03 prob 05

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -1,4 +1,34 @@
 package prob05;
 
 public class Account {
+	private String accountNo;
+	private int balance;
+	
+	public Account(String no) {
+		accountNo = no;
+	}
+	
+	public void save(int mon) {
+		balance += mon;
+	}
+	
+	public void deposit(int mon) {
+		balance -= mon;
+	}
+	
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
+
 }
