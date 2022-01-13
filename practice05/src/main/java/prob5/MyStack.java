62fb773f6cea95d2a9acc83c22c5624b757b0157
fascinate98 <fascinate982@naver.com>
2022-01-13 17:04:27 +0900

    practice05 prob05

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,38 @@
 package prob5;
 
 public class MyStack {
+	private String[] stack ;
+	private int num;
+	
+	public MyStack(int i) {
+		stack = new String[i];
+	}
+	
+	public void push(String msg) {
+		if(num>= stack.length) {
+			System.out.println("스택 오버 플로우");
+		}else {
+			stack[num++]=msg;			
+		}
+	}
+	
+	public String pop() throws MyStackException  {
+		
+		if(num>0) {
+			return stack[--num];
+		}else {
+			throw new MyStackException("stack is empty");
+		}
+	}
+	
+	public boolean isEmpty() {
+		if(stack.length == 0) {
+			return true;
+		}
+		else
+		{
+			return false;
+			
+		}
+	}
 }
---
+++ practice05/src/main/java/prob5/MyStackException.java
@@ -0,0 +1,11 @@
+package prob5;
+
+public class MyStackException extends Exception{
+	MyStackException(String msg){// 문자열을 매개변수로 받는 생성자
+
+		super(msg);// 조상인 Exception 클래스의 생성자를 호출한다.
+
+	}
+
+
+}
