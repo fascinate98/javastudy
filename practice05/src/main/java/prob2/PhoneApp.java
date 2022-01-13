e661d75775d2204d4d9d0ab85e7f942fdab038b7
fascinate98 <fascinate982@naver.com>
2022-01-13 16:17:01 +0900

    practice05 prob02

--- practice05/src/main/java/prob2/PhoneApp.java
+++ practice05/src/main/java/prob2/PhoneApp.java
@@ -1,12 +1,12 @@
 package prob2;
 
-public class PhoneApp {
+public class PhoneApp extends MusicPhone {
 
 	public static void main(String[] args) {
-//		Phone phone = new SmartPhone();
-//
-//		phone.execute("음악");
-//		phone.execute("통화");
-//		phone.execute("앱");
+		Phone phone = new SmartPhone();
+
+		phone.execute("음악");
+		phone.execute("통화");
+		phone.execute("앱");
 	}
 }
